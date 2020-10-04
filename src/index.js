ac4d8ce13817e7c9195bbf0f4cbec4e2795ab983
Amine AMEZIANE <amezianeamine@gmail.com>
2020-10-04 15:21:30 +0100

    circleci config added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./views/Home";
+import Products from "./views/Products";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App>
+			<Router>
+				<Switch>
+					<Route path="/" exact>
+						<Home />
+					</Route>
+
+					<Route path="/products">
+						<Products />
+					</Route>
+				</Switch>
+			</Router>
+		</App>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
