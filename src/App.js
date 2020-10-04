dee9f8d4287deab347d5f76cadadaab5f5ad8394
Amine AMEZIANE <amezianeamine@gmail.com>
2020-10-04 15:43:35 +0100

    update app test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 
 const App = (props) => {
-	return <div className="App">{props.children}</div>;
+	return (
+		<div className="App">
+			<h2>Store website under construction</h2>
+			{props.children}
+		</div>
+	);
 };
 
 export default App;
